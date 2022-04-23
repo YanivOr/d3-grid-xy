a051a5f14b2cd26c66d4adf94878b64448d7df7a
Yaniv Or <yaniv.or.78@gmail.com>
2022-04-23 15:25:02 +0300

    scale

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-const data = [...Array(1000).keys()];
+const data = [...Array(300).keys()]; // each unit is a millimeter
 
 (() => {
   new Container({
     x: 0,
     y: 0,
-    width: 700,
-    height: 700,
+    width: 900,
+    height: 900,
     data,
   });
 })();
